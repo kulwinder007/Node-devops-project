94c000c229be36ed0e2fdae0b41511fecd3dd88e
kulwinder007 <kulwindersingh915.ca@gmail.com>
2025-04-13 01:50:30 -0400

    healthcheck

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const redisClient = new Redis(process.env.REDIS_URL);
-
+app.get('/health', (req, res) => res.sendStatus(200));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
